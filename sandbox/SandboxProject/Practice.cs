c8d42cfcabce78c0fbce7619090372eeba081cf8
Robert <rramirez@arkus-solutions.com>
2022-10-23 22:08:56 -0700

    practicas

---
+++ sandbox/SandboxProject/Practice.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace SandBox
+{
+    class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+
+
+    }
+
+}
